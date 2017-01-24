835cb42e61429d23daa489783f4f2139f6b1e459
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-01-24 17:48:07 +0200

    Require apidoc/apidoc, write documentation and ignore generated documentation at public/docs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,30 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    /**
+     * @apiGroup            Authentication
+     * @apiName             Signin
+     * @apiDescription      Sign in to the application.
+     * @api                 {post} /signin Sign in
+     * @apiParam            {string} username Username of the user.
+     * @apiParam            {string} password Password of the user.
+     * @apiSuccess          (200) {string} message Success message.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+     *                              {
+     *                                  "message": "You have been signed in.",
+     *                                  "api_token": "3fb75d3744bebfe78965c5a9e82963832c3ad4744dc03e9188c5c982d85978ef179324162d7e2e088963409036fb4a460ef7c05f6e828b80b384f0b246fde5dff31ee7451ac83eeb9b891d4e973c918162c2aedceeaaab80399f0c137c31f8e56cb9b2456561208c6f479cb2a11bbacad1e9b80b645ae6edfdd32b9eab27a08b"
+     *                              }
+     * @apiError            (401) {string} message Error message.
+     * @apiErrorExample     {json} Error-Response:
+     *                          HTTP/1.1 401 Unauthorized
+     *                              {
+     *                                  "message": "Signin attempt failed.",
+     *                              }
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function signin(Request $request)
     {
         $user = User::where('username', $request->input('username'))->first();
@@ -33,6 +57,21 @@ class AuthController extends Controller
         return response()->json(['message' => $user->api_token], 401);
     }
 
+    /**
+     * @apiGroup            Authentication
+     * @apiName             Signout
+     * @apiDescription      Sign out of the application.
+     * @api                 {post} /signin Sign out
+     * @apiSuccess          (200) {string} message Success message.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+     *                              {
+     *                                  "message": "You have been signed out.",
+     *                              }
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function signout(Request $request)
     {
         $user = User::where('api_token', $request->header('x-access-token'))->first();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,10 @@ class UsersController extends Controller
         //
     }
 
+    /**
+     * @param $identifier
+     * @return mixed
+     */
     private function getUserInstance($identifier)
     {
         if (is_numeric($identifier)) {
@@ -27,31 +31,171 @@ class UsersController extends Controller
         }
     }
 
+    /**
+     * @apiGroup            Users
+     * @apiName             GetAllUsers
+     * @apiDescription      Get a list of all users.
+     * @api                 {get} /users Get all users
+     * @apiSuccess          (200) {array} message List of users.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+     *                              [
+                                        {
+                                            "id": 1,
+                                            "username": "user1",
+                                            "email": "user1@example.com",
+                                            "created_at": null,
+                                            "updated_at": "2017-01-24 14:37:19"
+                                        },
+                                        {
+                                            "id": 2,
+                                            "username": "user2",
+                                            "email": "user2@example.com",
+                                            "created_at": null,
+                                            "updated_at": null
+                                        }
+                                    ]
+
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function all()
     {
         return User::all();
     }
 
+    /**
+     * @apiGroup            Users
+     * @apiName             GetUser
+     * @apiDescription      Get a user by ID or username.
+     * @api                 {get} /users/:identifier Get a user
+     * @apiParam            {string} identifier User ID or username.
+     * @apiSuccess          (200) {string} message User object.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+                                    {
+                                        "id": 1,
+                                        "username": "user1",
+                                        "email": "user1@example.com",
+                                        "created_at": null,
+                                        "updated_at": "2017-01-24 14:37:19"
+                                    }
+     *
+     * @param $identifier
+     * @return mixed
+     */
     public function get($identifier)
     {
         return $this->getUserInstance($identifier);
     }
 
+    /**
+     * @apiGroup            Users
+     * @apiName             GetMediaOfUser
+     * @apiDescription      Get a list of media uploaded by a user.
+     * @api                 {get} /users:identifier Get media of a user
+     * @apiParam            {string} identifier User ID or username.
+     * @apiSuccess          (200) {json} array List of media.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+                                    [
+                                        {
+                                        "id": 1,
+                                        "user_id": 1,
+                                        "group_id": null,
+                                        "file_name": "test1.jpg",
+                                        "title": "Test Image 1",
+                                        "description": "This is the first test image.",
+                                        "tag": "C++",
+                                        "media_type": "image",
+                                        "mime_type": "image/jpeg",
+                                        "group_priority": null,
+                                        "created_at": null,
+                                        "updated_at": null
+                                        }
+                                    ]
+     *
+     * @param $identifier
+     * @return mixed
+     */
     public function getMedia($identifier)
     {
         return $this->getUserInstance($identifier)->media;
     }
 
+    /**
+     * @apiGroup            Users
+     * @apiName             GetGroupsOfUser
+     * @apiDescription      Get a list of groups created by a user.
+     * @api                 {get} /users:identifier/groups Get groups of a user
+     * @apiParam            {string} identifier User ID or username.
+     * @apiSuccess          (200) {array} array List of groups.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+                                    [
+                                        {
+                                            "id": 1,
+                                            "user_id": 1,
+                                            "name": "My PHP Tutorials",
+                                            "tag": "PHP",
+                                            "created_at": null,
+                                            "updated_at": null
+                                        }
+                                    ]
+     *
+     * @param $identifier
+     * @return mixed
+     */
     public function getGroups($identifier)
     {
         return $this->getUserInstance($identifier)->groups;
     }
 
+    /**
+     * @apiGroup            Users
+     * @apiName             GetCommentsOfUser
+     * @apiDescription      Get a list of comments posted by a user.
+     * @api                 {get} /users:identifier/comments Get comments of a user
+     * @apiParam            {string} identifier User ID or username.
+     * @apiSuccess          (200) {array} message List of comments.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+                                    [
+                                        {
+                                            "id": 1,
+                                            "user_id": 1,
+                                            "medium_id": 2,
+                                            "comment": "The first comment.",
+                                            "created_at": null,
+                                            "updated_at": null
+                                        }
+                                    ]
+     *
+     * @param $identifier
+     * @return mixed
+     */
     public function getComments($identifier)
     {
         return $this->getUserInstance($identifier)->comments;
     }
 
+    /**
+     * @apiGroup            Users
+     * @apiName             CreateUser
+     * @apiDescription      Create a user.
+     * @api                 {post} /users Create a user
+     * @apiParam            {string} username Username of the user.
+     * @apiParam            {string} Password Password of the user.
+     * @apiParam            {string} email Email address of the user.
+     * @apiSuccess          (200) {json} message Success message
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 201 Created
+                                    {
+                                        "message": "User user1 has been created."
+                                    }
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function post(Request $request)
     {
         try {
