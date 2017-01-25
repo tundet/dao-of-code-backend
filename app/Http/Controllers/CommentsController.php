d8c82a6aab84089de48a53aacf0cc5312731a234
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-01-25 09:52:11 +0200

    Write documentation for comments

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -18,23 +18,90 @@ class CommentsController extends Controller
         //
     }
 
+    /**
+     * @apiGroup            Comments
+     * @apiName             GetAllComments
+     * @apiDescription      Get a list of all comments.
+     * @api                 {get} /comments Get all comments
+     * @apiSuccess          (200) {array} message List of comments.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+     *                              [
+                                        {
+                                            "id": 1,
+                                            "user_id": 1,
+                                            "medium_id": 2,
+                                            "comment": "The first comment.",
+                                            "created_at": null,
+                                            "updated_at": null
+                                        },
+                                        {
+                                            "id": 2,
+                                            "user_id": 2,
+                                            "medium_id": 3,
+                                            "comment": "The second comment.",
+                                            "created_at": null,
+                                            "updated_at": null
+                                        }
+                                    ]
+
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function all(Request $request)
     {
         return Comment::all();
     }
 
+    /**
+     * @apiGroup            Comments
+     * @apiName             GetComment
+     * @apiDescription      Get a comment by ID.
+     * @api                 {get} /comments/:id Get a comment
+     * @apiParam            {string} id ID of the comment.
+     * @apiSuccess          (200) {string} message Comment object.
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 200 OK
+                                {
+                                    "id": 1,
+                                    "user_id": 1,
+                                    "medium_id": 2,
+                                    "comment": "The first comment.",
+                                    "created_at": null,
+                                    "updated_at": null
+                                }
+     *
+     * @param $identifier
+     * @return mixed
+     */
     public function get($identifier)
     {
         return Comment::findOrFail($identifier);
     }
 
+    /**
+     * @apiGroup            Comments
+     * @apiName             CreateComment
+     * @apiDescription      Create a comment.
+     * @api                 {post} /comments Create a comment
+     * @apiParam            {number} medium_id ID of the medium the comment belongs to.
+     * @apiParam            {string} Comment Contents of the comment.
+     * @apiSuccess          (200) {json} message Success message
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 201 Created
+                                {
+                                    "message": "The comment has been created."
+                                }
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function post(Request $request)
     {
         try {
             $comment = new Comment();
 
             $comment->user_id = User::where('api_token', $request->header('x-access-token'))->value('id');
-            $comment->file_id = $request->input('file_id');
+            $comment->medium_id = $request->input('medium_id');
             $comment->comment = $request->input('comment');
 
             $comment->save();
@@ -45,6 +112,20 @@ class CommentsController extends Controller
         }
     }
 
+    /**
+     * @apiGroup            Comments
+     * @apiName             DeleteComment
+     * @apiDescription      Delete a comment.
+     * @api                 {delete} /comments Delete a comment
+     * @apiSuccess          (200) {json} message Success message
+     * @apiSuccessExample   {json} Success-Response:
+     *                          HTTP/1.1 20 OK
+                                {
+                                    "message": "The comment has been deleted."
+                                }
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function delete($id)
     {
         try {
