9b02c8193e149f8c7e4c90d817e69efa378474e7
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-02-15 10:51:26 +0200

    Get file extension correctly

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -298,7 +298,7 @@ class MediaController extends Controller
 
             $medium->user_id = User::where('api_token', $request->header('x-access-token'))->value('id');
             $medium->group_id = $request->input('group_id');
-            $medium->file_name = $nextMediumId . '.' . $request->file('file')->guessExtension();
+            $medium->file_name = $nextMediumId . '.' . $request->file('file')->getExtension();
             $medium->title = $request->input('title');
             $medium->description = $request->input('description');
             $medium->tag = $request->input('tag');
