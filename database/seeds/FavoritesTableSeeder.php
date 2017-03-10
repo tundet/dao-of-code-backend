7d9d70f8785453323975befb47644a79bafbd9f4
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-03-10 14:30:39 +0200

    Add groups to favorites table seeder

--- database/seeds/FavoritesTableSeeder.php
+++ database/seeds/FavoritesTableSeeder.php
@@ -28,5 +28,20 @@ class FavoritesTableSeeder extends Seeder
             'user_id'       => 3,
             'medium_id'     => 1
         ]);
+
+        DB::table('favorites')->insert([
+            'user_id'       => 1,
+            'group_id'      => 2
+        ]);
+
+        DB::table('favorites')->insert([
+            'user_id'       => 2,
+            'group_id'      => 3
+        ]);
+
+        DB::table('favorites')->insert([
+            'user_id'       => 3,
+            'group_id'      => 1
+        ]);
     }
 }
