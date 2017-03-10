a28a4884a46d9b30833a8e18933d3c52c8e68ec4
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-03-10 09:29:08 +0200

    Make /get-usernames a POST request in the documentation

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -92,7 +92,7 @@ class UsersController extends Controller
      * @apiGroup            Users
      * @apiName             GetUsernames
      * @apiDescription      Get a list of usernames matching use IDs.
-     * @api                 {get} /get-usernames Get a list of usernames
+     * @api                 {post} /get-usernames Get a list of usernames
      * @apiParam            {string} id[] One or more usernames. Send the field multiple times to get multiple usernames.
      * @apiSuccess          (200) {array} message List of usernames.
      * @apiSuccessExample   {array} Success-Response:
