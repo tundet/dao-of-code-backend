453535f9359a90b28729464d5ac28c56601dcc07
Jukka Kumpunen <jukka.kumpunen@gmail.com>
2017-03-13 13:58:04 +0200

    Add real media to seeder

--- database/seeds/MediaTableSeeder.php
+++ database/seeds/MediaTableSeeder.php
@@ -14,62 +14,36 @@ class MediaTableSeeder extends Seeder
     public function run()
     {
         DB::table('media')->insert([
-            'user_id'        => 1,
-            'group_id'       => 1,
-            'file_name'      => '2.gif',
-            'title'          => 'Test Image 1',
-            'description'    => 'This is the first test image.',
+            'user_id'        => 3,
+            'group_id'       => 3,
+            'title'          => 'What is PHP?',
+            'description'    => 'PHP explained in 10 minutes.',
             'tag'            => 'php',
-            'media_type'     => 'image',
-            'mime_type'      => 'image/gif',
+            'media_type'     => 'youtube',
+            'youtube_url'    => 'https://www.youtube.com/embed/-oUXcTz1eEA',
             'group_priority' => 1
-        ]);
-
-        DB::table('media')->insert([
-            'user_id'        => 2,
-            'group_id'       => 2,
-            'file_name'      => '2.gif',
-            'title'          => 'Test Image 2',
-            'description'    => 'This is the second test image.',
-            'tag'            => 'javascript',
-            'media_type'     => 'image',
-            'mime_type'      => 'image/gif',
-            'group_priority' => 1
-        ]);
-
-        DB::table('media')->insert([
-            'user_id'        => 3,
-            'group_id'       => 3,
-            'file_name'      => '3.gif',
-            'title'          => 'Test Image 3',
-            'description'    => 'This is the third test image.',
-            'tag'            => 'cpp',
-            'media_type'     => 'image',
-            'mime_type'      => 'image/gif',
-            'group_priority' => 1
-        ]);
-
-        DB::table('media')->insert([
-            'user_id'        => 3,
-            'group_id'       => 3,
-            'file_name'      => '4.png',
-            'title'          => 'Test Image 3',
-            'description'    => 'This is the third test image.',
-            'tag'            => 'cpp',
-            'media_type'     => 'image',
-            'mime_type'      => 'image/png',
-            'group_priority' => 2
         ]);
 
         DB::table('media')->insert([
             'user_id'        => 3,
             'group_id'       => 3,
             'file_name'      => '5.jpg',
-            'title'          => 'Test Image 3',
-            'description'    => 'This is the third test image.',
-            'tag'            => 'cpp',
-            'media_type'     => 'image',
-            'mime_type'      => 'image/jpeg',
+            'title'          => 'Learn PHP in 30 minutes',
+            'description'    => 'Learn the basics of PHP in just 30 minutes!',
+            'tag'            => 'php',
+            'media_type'     => 'youtube',
+            'youtube_url'    => 'https://www.youtube.com/embed/7TF00hJI78Y',
+            'group_priority' => 2
+        ]);
+
+        DB::table('media')->insert([
+            'user_id'        => 3,
+            'group_id'       => 3,
+            'title'          => 'Laravel Podcast #26',
+            'description'    => 'Discussing Laravel 5.3.8, Spark 2 and Lumen 5.3.',
+            'tag'            => 'php',
+            'media_type'     => 'youtube',
+            'youtube_url'    => 'https://www.youtube.com/embed/2m7IY-Xb6Og',
             'group_priority' => 3
         ]);
     }
